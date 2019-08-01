57aca9f64552d04ce5cb4a151e439920b38b3030
Claudia Seldner <claudiaseldner@Claudias-MacBook-Pro-2.local>
2019-07-31 20:18:36 -0500

    Add SQL Schemas and file, config and server.js

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Five Star');
+INSERT INTO burgers (burger_name) VALUES ('Cow Boy');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Sonora', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Ranch', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', true);
+INSERT INTO burgers (burger_name) VALUES ('Big Carl');
